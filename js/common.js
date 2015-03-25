1a6a14de0d35febac43a81f122fc8a43727d219c
m-kozelchuk <marina.kozelchuk@gmail.com>
2015-03-25 19:51:17 +0200

    fixes 24.03

--- js/common.js
+++ js/common.js
@@ -1,19 +1,8 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	$(".js-menu-mobile").click(function(){
+		$(".main-menu").toggleClass("active");
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	});
 
-	console.log($('body').html());
 });
