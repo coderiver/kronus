47c918211e3bc06b2ef116b38344c7a3e8c4eace
m-kozelchuk <marina.kozelchuk@gmail.com>
2015-03-27 16:55:17 +0200

    fixes 27.03

--- js/common.js
+++ js/common.js
@@ -1,8 +1,18 @@
 head.ready(function() {
 
 	$(".js-menu-mobile").click(function(){
-		$(".main-menu").toggleClass("active");
-
+		$(".main-menu").addClass("active");
+		$(".menu-button").addClass("active");
+		$(".js-menu-mobile>p").css("display", "none");
+		$(".js-menu-close").css("display", "block");
+		return false;
+	});
+	$(".js-menu-close").click(function(){
+		$(".main-menu").removeClass("active");
+		$(".menu-button").removeClass("active");
+		$(".js-menu-mobile>p").css("display", "block");
+		$(".js-menu-close").css("display", "none");
+		return false;
 	});
 
 });
