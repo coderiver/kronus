42e12173256871a1fe4b6ad9ce4febf65bc8578d
m-kozelchuk <marina.kozelchuk@gmail.com>
2015-04-02 18:05:38 +0300

    upd 02.04

--- js/common.js
+++ js/common.js
@@ -1,4 +1,40 @@
-head.ready(function() {
+jQuery(document).ready(function($) {
+		
+
+	var mainmenu = $('.main-menu');
+
+    if ( mainmenu.length ) {
+
+        var itemSelector    = '.main-menu__item',
+            btnSelector     = '.main-menu__btn',
+            contentSelector = '.sub-menu',
+            activeClass     = 'active';
+
+        mainmenu.each(function() {
+            var el       = $(this),
+                btn      = el.find(btnSelector),
+                items    = el.find(itemSelector),
+                contents = el.find(contentSelector);
+
+            btn.on('click', function(event) {
+                event.preventDefault();
+
+                var currentBtn = $(this),
+                    item       = currentBtn.parent(itemSelector),
+                    content    = currentBtn.siblings(contentSelector);
+
+                if ( item.hasClass(activeClass) ) {
+                    item.removeClass(activeClass);
+                    content.slideUp(200);
+                } else {
+                    items.removeClass(activeClass);
+                    contents.slideUp(200);
+                    item.addClass(activeClass);
+                    content.slideDown(200);
+                }
+            });
+        });
+    }
 
 	$(".js-menu-mobile").click(function(){
 		$(".main-menu").addClass("active");
