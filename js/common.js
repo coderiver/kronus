49cb56e2e5e0ccec83b4fca53adb8086d30b3586
m-kozelchuk <marina.kozelchuk@gmail.com>
2015-04-24 18:06:47 +0300

    24.04 in progres

--- js/common.js
+++ js/common.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($) {
 
         var itemSelector    = '.main-menu__item',
             btnSelector     = '.main-menu__btn',
-            contentSelector = '.sub-menu',
+            contentSelector = '.mob-sub-menu',
             activeClass     = 'active';
 
         mainmenu.each(function() {
